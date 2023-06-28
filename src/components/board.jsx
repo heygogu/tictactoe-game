05db086c6b40f5c9d0c59dea0e35332e52021f5b
heygogu <rohitnarnolia88@gmail.com>
2023-06-28 07:15:18 +0530

    Introduced a player to the game.SHow either 0 or x after clicking

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -3,17 +3,26 @@ import Square from './square';
 import { useState } from 'react';
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
-
+  const [isXNext,setIsXNext]=useState(false);
+  
   const handleSquareClick = Clickedposition => {
+
+    if(squares[Clickedposition]){
+      return;
+    }
+
+
     setSquares(currentSquares => {
       // currentSquares[0]=1; we can't directly mutate the values, also position in next line have a meaning
       return currentSquares.map((squareValue, position) => {
         if (Clickedposition == position) {
-          return 'X';
+          return isXNext ? 'X' : "0";
         }
         return squareValue;
       });
     });
+
+    setIsXNext(currentIsXNext => !currentIsXNext);
   };
 
   const renderSquare = position => {
