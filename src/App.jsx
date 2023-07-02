37cf40290610df2128ad96b86ee24082027819d3
heygogu <rohitnarnolia88@gmail.com>
2023-07-02 20:36:35 +0530

    Added Font Title and included Roboto Font

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
   const gamingBoard = history[currentMove];
 
-  const {winner,winningSquares} = calculateWinner(gamingBoard.squares);
+  const { winner, winningSquares } = calculateWinner(gamingBoard.squares);
   const handleSquareClick = Clickedposition => {
     if (gamingBoard.squares[Clickedposition] || winner) {
       return;
@@ -60,6 +60,9 @@ function App() {
   };
   return (
     <div className="app">
+      <h1>
+        TIC <span className="text-green">TAC</span> TOE
+      </h1>
       <StatusMessage winner={winner} gamingBoard={gamingBoard} />
       <Board
         squares={gamingBoard.squares}
@@ -73,7 +76,13 @@ function App() {
       >
         Start New Game{' '}
       </button>
-      <h2>Current Game History</h2>
+      <h2
+        style={{
+          fontWeight: 'normal',
+        }}
+      >
+        Current Game History
+      </h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
